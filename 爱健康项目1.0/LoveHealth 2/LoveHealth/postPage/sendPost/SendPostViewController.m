6211450aa09499490a58af396b2e6e18ba755dfd
chenwenkai1993 <1620675919@qq.com>
2016-04-06 10:08:05 +0800

    temp1

--- 爱健康项目1.0/LoveHealth 2/LoveHealth/postPage/sendPost/SendPostViewController.m
+++ 爱健康项目1.0/LoveHealth 2/LoveHealth/postPage/sendPost/SendPostViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "SendPostViewController.h"
-
+#import "Header.h"
 @interface SendPostViewController ()
+
+@property (strong,nonatomic)UILabel *titleLabel;
+
+@property (strong,nonatomic)UITextField *titleText;
+
+@property (strong,nonatomic)UILabel *contentLabel;
+
+@property (strong,nonatomic)UITextField *contentText;
+
+@property (strong,nonatomic)UIButton *markBtn;
 
 @end
 
@@ -17,11 +27,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.navigationItem.title = @"发帖子";
+    
+    [self createPart];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)createPart{
+    
+    self.titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, 80, 35, 20)];
+    self.titleLabel.text = @"标题:";
+    [self.view addSubview:self.titleLabel];
+    
+    self.titleText = [[UITextField alloc]initWithFrame:CGRectMake(15, 115, 290, 30)];
+    self.titleText.layer.cornerRadius = 6;
+    [self.view addSubview:self.titleText];
+    
+    self.contentLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, 155, 35, 20)];
+    self.contentLabel.text = @"内容:";
+    [self.view addSubview:self.contentLabel];
+    
+    self.contentText = [[UITextField alloc]initWithFrame:CGRectMake(15, 185, 290, 305)];
+    self.contentText.layer.cornerRadius = 6;
+    [self.view addSubview:self.contentText];
+    
+    self.markBtn = [[UIButton alloc]initWithFrame:CGRectMake(220, 80, 84, 20)];
+    [self.markBtn setBackgroundColor:COLOR(204, 255, 255, 1)];
+    [self.markBtn setTitle:@"点击选择标签" forState:UIControlStateNormal];
+    [self.markBtn set]
 }
 
 /*
